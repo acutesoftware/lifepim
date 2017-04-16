6d919f3f66f2b244827ed749ad769e3169f471f4
Duncan Murray <djmurray@gmail.com>
2017-04-16 22:54:51 +0930

    updated python version

--- lifepim/go_web_lifepim.bat
+++ lifepim/go_web_lifepim.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting web server for LifePIM
 
-start /min C:\python34\python.exe web_app\web_lifepim.py
+start /min C:\Python36-32\python.exe web_app\web_lifepim.py
 echo Server started - press SPACE to start browser
 pause
 start  http://127.0.0.1:5000
