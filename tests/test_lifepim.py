0c7a0e9b7b72130d911fb6d064ba2a95b84125fc
acutesoftware <djmurray@acutesoftware.com.au>
2018-10-27 00:07:07 +1030

    test to choose local or live site

--- tests/test_lifepim.py
+++ tests/test_lifepim.py
@@ -5,21 +5,28 @@ import os
 import sys
 import unittest
 
-root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." )
 pth = root_folder + os.sep + 'lifepim'
 sys.path.append(pth)
 
 import lifepim as mod_lp
 
+local_site = 'http://127.0.0.1:5000'
+live_site = 'https://www.lifepim.com'
+
+#site_to_test = local_site
+site_to_test = live_site
+
+
 class TestLifePIM(unittest.TestCase):
-    
+
     def test_01_connect(self):
         res = mod_lp.connect()
         self.assertEqual(res, 200)
 
     def test_02_valid_page(self):
-        
-        c = mod_lp.LifePimConnect('https://www.lifepim.com','')
+
+        c = mod_lp.LifePimConnect(site_to_test,'')
         res = c.get_page('/about', 200)
         self.assertTrue(len(res) > 500)
 
@@ -27,8 +34,8 @@ class TestLifePIM(unittest.TestCase):
 
 
     def test_03_no_such_page(self):
-        
-        c = mod_lp.LifePimConnect('https://www.lifepim.com','')
+
+        c = mod_lp.LifePimConnect(site_to_test,'')
         res = c.get_page('/blahBLAH', 200)
         if res:
             self.assertEqual('this should have failed', 'YES')
@@ -37,12 +44,12 @@ class TestLifePIM(unittest.TestCase):
 
 
     def test_04_API_public(self):
-        c = mod_lp.LifePimConnect('http://127.0.0.1:5000', '')
+        c = mod_lp.LifePimConnect(site_to_test, '')
         #print(c)
         #http://127.0.0.1:5000/API/V1/get_note/fsdf
         res = c.get_page('/API/V1/get_note/My Note', '200')
         print(res)
 
-    
+
 if __name__ == '__main__':
     unittest.main()
