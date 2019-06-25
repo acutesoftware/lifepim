e2eb5a8cbdf6687a9200681901fd0d89e5281820
acutesoftware <djmurray@acutesoftware.com.au>
2019-06-25 21:42:09 +0930

    setting up source folder

---
+++ .z_prototype/personal_config.py
@@ -0,0 +1,11 @@
+# configutation setup for LifePIM Desktop
+# ---------------------------------------
+# LifePIM Desktop runs locally on your system.
+# Modify the settings in this file with your own
+# folder locations and logons
+
+folder_home=/home/acutesoftware
+
+
+logon_username=Test
+logon_password=hunter2
