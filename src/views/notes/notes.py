7b168d0181d329ee7c65213d39e23313eed83fcf
acutesoftware <djmurray@acutesoftware.com.au>
2019-08-08 23:38:51 +0930

    initial notes view

---
+++ src/views/notes/notes.py
@@ -0,0 +1,18 @@
+# notes.py
+
+from tkinter import Frame, Label, Entry, Button, font
+from tkinter import LEFT, RIGHT, TOP, BOTTOM
+
+def build_screen_notes(root):
+    console_frame = Frame(root, bg='white', width=850, height=250, pady=1)
+    console_frame.grid(row=0, column=0, sticky="nesw")
+
+
+
+    txt = 'welcome to notes\n'
+    console_label = Label(root, text=txt, bg='black', foreground='green', justify=LEFT)
+    console_label.grid(row=0, column=0)
+    console_entry = Entry(root,  bg='gray12', foreground='green')
+    console_entry.grid(row=1, column=0, sticky="ew")
+
+
