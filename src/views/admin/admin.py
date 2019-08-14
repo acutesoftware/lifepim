db6216163effad5cda83c881a204a2353c96fa70
acutesoftware <djmurray@acutesoftware.com.au>
2019-08-14 22:16:38 +0930

    admin screen gets local PC list on network

--- src/views/admin/admin.py
+++ src/views/admin/admin.py
@@ -1,18 +1,29 @@
 # notes.py
 
+import os
 from tkinter import Frame, Label, Entry, Button, font
 from tkinter import LEFT, RIGHT, TOP, BOTTOM
 
 def build_screen(root):
-    console_frame = Frame(root, bg='white', width=850, height=250, pady=1)
-    console_frame.grid(row=0, column=0, sticky="nesw")
     print('building screen_admin...')
 
 
-    txt = 'Admin Page\n'
+    txt = 'Admin Page\n' + get_local_pc_list()
+
     console_label = Label(root, text=txt, bg='white', foreground='blue', justify=LEFT)
     console_label.grid(row=0, column=0)
     console_entry = Entry(root,  bg='gray12', foreground='green')
     console_entry.grid(row=1, column=0, sticky="ew")
 
 
+
+def get_local_pc_list():
+    txt = ''
+    out = os.popen('ip neigh').read().splitlines()
+    for i, line in enumerate(out, start=1):
+        ip = line.split(' ')[0]
+        h = os.popen('host {}'.format(ip)).read()
+        hostname = h.split(' ')[-1]
+        txt += ip + ' ' + hostname.strip() + '\n'
+    return txt
+    
