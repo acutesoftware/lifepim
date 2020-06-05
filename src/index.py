8f53470c99428b36fc5f089f5643c7a365c6acdf
Duncan Murray <djmurray@gmail.com>
2020-06-05 22:00:01 +0930

    sample data files and index outline

---
+++ src/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding: utf-8
+# index.py
+
+import os
+import config as mod_cfg
+
+def build_indexes():
+    print('rebuilding indexes in ', mod_cfg.index_folder)
+    print('scanning ', mod_cfg.data_folder)
+
+
+
+
+if __name__ == '__main__':
+    build_indexes()
+
