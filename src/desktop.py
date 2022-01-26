73eb8736d2e5b18d33304d1c1dde1a29fbabea1f
Duncan Murray <djmurray@gmail.com>
2022-01-26 16:02:02 +1030

    file browser works and loads a text file to text editor

--- src/desktop.py
+++ src/desktop.py
@@ -7,6 +7,7 @@ import sys
 
 from PyQt5.QtCore import QDate
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QDir
 from PyQt5.QtWidgets import QFrame
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QSplitter
@@ -20,13 +21,16 @@ from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QCalendarWidget
 from PyQt5.QtWidgets import (QTreeWidget, QTreeWidgetItem)
+from PyQt5.QtWidgets import QMessageBox
+
+from PyQt5.QtWidgets import (QFileSystemModel)
+from PyQt5.QtWidgets import (QTreeView, QListView)
 
 from PyQt5.QtGui import QIcon
 
 
 from interfaces import lp_screen
 import config as mod_cfg
-
 
 
 def main():
@@ -39,7 +43,15 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
 
     def __init__(self):
         super().__init__()
+
+        # global references across all focus modes
+        self.MainTextEditor = None
+        self.MainWidgetFilelist = None
+        self.currentFile = ''
+
         self.build_gui()
+
+
 
 
     def build_gui(self):
@@ -60,8 +72,7 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
         self.setCentralWidget(rootWidget)
 
         self._build_menu_and_toolbar(theme)
-
-        self.build_main_layout(rootWidget)
+        self._build_main_layout(rootWidget)
         self.statusBar().showMessage('Ready')
 
       
@@ -127,7 +138,7 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
 
 
 
-    def build_main_layout(self, rootWidget):
+    def _build_main_layout(self, rootWidget):
 
         # Step 1 - make the splitter interface
         rootBox = QHBoxLayout(self)
@@ -137,7 +148,7 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
         lblLeftBottom = QLabel(' Left Bottom - Files')
         lblCentre = QLabel(' Centre - NOTES')
         lblRight = QLabel(' Right - quick jump')
-        textEdit = QTextEdit()
+        
 
         leftTop = QFrame(self)
         leftTop.setFrameShape(QFrame.StyledPanel)
@@ -158,7 +169,7 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
         mid.setFrameShape(QFrame.StyledPanel)
         mid.resize(500,800)
         lblCentre.setParent(mid)
-        textEdit.setParent(mid)
+        
 
         right = QFrame(self)
         right.setFrameShape(QFrame.StyledPanel)
@@ -177,21 +188,20 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
         splitter2.addWidget(mid)
         splitter2.addWidget(right)
 
-
+        # ------------------------------------------------------------------------------------
         # add the components to the layout
         self.create_widget_calendar().setParent(leftTop)
         self.create_widget_treeview().setParent(leftMid)
+        self.create_widget_text_editor().setParent(mid)
+        self.create_widget_filelist().setParent(leftBottom)
+
+        # -----------------------------------------------------------------------------------
         #my_cal.setParent(leftTop)     
 
         # finally add the main horiz splitter to the root
         rootBox.addWidget(splitter2)
         rootWidget.setLayout(rootBox)    
         
-
-
-    def build_frame_left(self):
-        pass
-
 
 
     def make_toolbar_button(self, toolbar, theme, name, icon_name, cmd_name, shortcut, tooltip):
@@ -207,15 +217,86 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
 
     def get_run_command(self, cmd_name, param=[]):
         """
-        high level function to parse commands to correct module 
+        high level function to parse commands to correct module.
+        Tempted to use pass by name, but this might cause issues
+        if users mess up settings.cfg (or try to do injections 
+        of sys commands), so just hard code maps from strings to
+        action commands
         """
-        if cmd_name == 'quit':
-            return self.close
+        valid_commands = {
+            'quit':self.actExit,
+            'dummy':self.actDummy,
+            'home':self.actHome,
+            'addr':self.actAddr,
+            'bell':self.actBell,
+            'book':self.actBook,
+            'bookshelf':self.actBookshelf,
+            'chalkboard':self.actChalkboard,
+            'camera':self.actCamera,
+            'search':self.actSearch,
+            'cut':self.actCut,
+            'fix':self.actFix,
+            'dig':self.actDig,
+            'cal':self.actCal
+        }
 
-        return self.dummy
+        return valid_commands[cmd_name]
 
-    def dummy(self):  
+
+
+    def actExit(self):  
+        reply = QMessageBox.question(self, 'Exit LifePIM', 'Are you sure you want to close the window?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            self.close()
+            print('Window closed')
+        else:
+            pass
+        self.close
+
+    def actDummy(self):  
         print('running dummy command ' )  
+    def actCal(self):  
+        print('running calendar command ' )  
+    def actHome(self):  
+        print('running home command ' )  
+    def actAddr(self):  
+        print('running Addr command ' )  
+
+    def actAddr(self):  
+        print('running Addr command ' )  
+
+    def actAddr(self):  
+        print('running Addr command ' )  
+
+    def actBell(self):  
+        print('running Bell command ' )  
+
+    def actBook(self):  
+        print('running Book command ' )  
+
+    def actBookshelf(self):  
+        print('running Bookshelf command ' )  
+
+    def actChalkboard(self):  
+        print('running Chalkboard command ' )  
+
+    def actCamera(self):  
+        print('running Image command ' )  
+
+    def actSearch(self):  
+        print('running Search command ' )  
+
+    def actCut(self):  
+        print('running Data Cut command ' )  
+
+    def actFix(self):  
+        print('running Data Fix command ' )  
+
+    def actDig(self):  
+        print('running Data Dig command ' )  
+        self.statusBar().showMessage(str(glbMainTextEditor))
+
 
 
     def create_widget_calendar(self):
@@ -234,6 +315,7 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
         #self.lbl = QLabel(self)
         #self.lbl.setText(date.toString())
         #vbox.addWidget(self.lbl)
+        self.glbCal = cal
         return cal
         #return vbox
     def showDate(self, date):
@@ -256,9 +338,70 @@ class LifePIM_GUI(QMainWindow):   # works for menu and toolbar as QMainWindow
                 child.setText(0, "Child {}".format(x))
                 child.setCheckState(0, Qt.Unchecked)
         tree.show()         
-        
+        self.glbTree = tree
         return tree
 
+    def create_widget_text_editor(self):
+        self.MainTextEditor = QTextEdit(self)
+        self.MainTextEditor.resize(600,600)
+        
+        return self.MainTextEditor
+
+    def create_widget_filelist(self):
+        self.MainWidgetFilelist = FileWidget()
+        #self.MainWidgetFilelist.resize(600,600)
+        self.MainWidgetFilelist.attach_parent_reference(self)
+
+        return self.MainWidgetFilelist
+
+
+class FileWidget(QWidget):
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self, *args, **kwargs)
+        hlay = QHBoxLayout(self)
+        self.treeview = QTreeView()
+        self.listview = QListView()
+        
+        hlay.addWidget(self.treeview)
+        hlay.addWidget(self.listview)
+
+        path = QDir.rootPath()
+
+        self.dirModel = QFileSystemModel()
+
+        pth = mod_cfg.file_startup_path  # r"D:\dev"
+        self.dirModel.setRootPath(pth)
+        self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
+
+        self.fileModel = QFileSystemModel()
+        self.fileModel.setFilter(QDir.NoDotAndDotDot |  QDir.Files)
+
+        self.treeview.setModel(self.dirModel)
+        self.listview.setModel(self.fileModel)
+
+        self.treeview.setRootIndex(self.dirModel.index(pth))  # was path but defaults to C:
+        self.listview.setRootIndex(self.fileModel.index(pth)) # was path but defaults to C:
+
+        self.treeview.clicked.connect(self.on_clicked_folder)
+        self.listview.clicked.connect(self.on_clicked_file)
+
+    def attach_parent_reference(self, parentGui):
+        """
+        TODO - this is a terrible idea, but I havent worked out PyQT signals yet
+        """
+        self.MainGUI = parentGui
+
+    def on_clicked_folder(self, index):
+        path = self.dirModel.fileInfo(index).absoluteFilePath()
+        self.listview.setRootIndex(self.fileModel.setRootPath(path))
+
+    def on_clicked_file(self, index):
+        self.MainGUI.currentFile = self.fileModel.filePath(index)
+        print('view ing ' + self.MainGUI.currentFile)
+        text=open(self.MainGUI.currentFile).read()
+        self.MainGUI.MainTextEditor.setText(text)
+        
+        
 
 
 
